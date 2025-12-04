a01899e2244075b7145b2fdd1fa226cd82f6ba41
Patrick Oliver <poliver24@hotmail.com>
2025-12-04 16:54:23 +0000

    FEAT: added Index and NotFound pages

---
+++ src/pages/Index.tsx
@@ -0,0 +1,11 @@
+const Index = () => {
+    return (
+        <div className="min-h-screen bg-background">
+            <main>
+                portfolio page
+            </main>
+        </div>
+    );
+};
+
+export default Index;
