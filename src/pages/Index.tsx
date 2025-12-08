995464d7549e851ac6aa1c0e245c2afba9e9bd8b
Patrick Oliver <poliver24@hotmail.com>
2025-12-08 17:16:16 +0000

    FEAT: add website hero

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,10 +1,12 @@
 import { Navbar } from "@/components/portfolio/NavBar";
+import { Hero } from "@/components/portfolio/Hero";
 
 const Index = () => {
     return (
         <div className="min-h-screen bg-background">
             <Navbar />
             <main>
+                <Hero />
             </main>
         </div>
     );
